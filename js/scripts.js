345a11dccf315d9699f849a8569248bc926d60a8
Alejandro Marín Gómez <alejandromaringomez@hotmail.com>
2018-11-16 20:33:17 +0100

    Mejora para el funcionamiento básico del menú

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-	$('body > header a.icon').click(function() {
-		$('body > header > nav').show('slide');
-	));
+	
+	// Menu principal
+	
+	$('#icono-menu').click(function (e) {
+		e.preventDefault();
+		if ($(this).hasClass('active')){
+			$(this).removeClass('active');
+			$('body > nav').fadeToggle('fast', 'linear');
+		} else {
+			$(this).addClass('active');
+			$('body > nav').fadeToggle('fast', 'linear');
+		}
+	});
+	
 });
