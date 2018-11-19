75f969533d52f4270413d4b762ecad69b60fd7b1
Alejandro Marín Gómez <alejandromaringomez@hotmail.com>
2018-11-19 21:37:51 +0100

    Mejorando el menu

--- js/scripts.js
+++ js/scripts.js
@@ -13,19 +13,26 @@ $(document).ready(function() {
 	// Menu principal
 	// Por Alejandro Marín Gómez
 	
-	$('.boton-menu-principal').click(function(e) {
+	$('a#AbrirMenuPrincipal').click(function(e) {
 		e.preventDefault();
-		let Boton = $(this);
 		let Menu = $('body > nav');
-		if (Menu.hasClass('active')){
-			$('body > div#overlay').remove();
-			Menu.removeClass('active');
-			Menu.fadeToggle('fast', 'linear');
-		} else {
-			$('body').prepend('<div id="overlay"></div>');
-			Menu.addClass('active');
-			Menu.fadeToggle('fast', 'linear');
-		}
+		let Body = $('body');
+		Menu.addClass('active');
+		Menu.fadeToggle('fast', 'linear', function() {
+			Body.prepend('<div id="nav_overlay" class="overlay CerrarMenuPrincipal" style="display: none;"></div>', function() {
+				$('div#nav_overlay').fadeInt(1200);
+			});
+		});
+		
+	});
+	
+	$('.CerrarMenuPrincipal').click(function(e) {
+		e.preventDefault();
+		let Menu = $('body > nav');
+		let Overlay = $('body > div#nav_overlay');
+		Menu.removeClass('active');
+		Menu.fadeToggle('fast', 'linear');
+		Overlay.fadeOut(1200);
 	});
 	
 });
