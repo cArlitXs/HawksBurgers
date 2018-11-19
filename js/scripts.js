37beda0592ee25baa979206fe392ac454429ae09
Alejandro Marín Gómez <alejandromaringomez@hotmail.com>
2018-11-19 21:51:00 +0100

    Mejora del menu

--- js/scripts.js
+++ js/scripts.js
@@ -20,14 +20,14 @@ $(document).ready(function() {
 		let Body = $('body');
 		Menu.addClass('active');
 		Menu.fadeToggle('fast', 'linear');
-		Body.prepend('<div id="nav_overlay" class="overlay CerrarMenuPrincipal" style="display: none;"></div>');
-		$('div#nav_overlay').fadeIn(1200);
+		Body.prepend('<a href="#" id="nav_overlay" class="overlay CerrarMenuPrincipal" style="display: none;"></a>');
+		$('a#nav_overlay').fadeIn(1200);
 	});
 	
-	$('.CerrarMenuPrincipal').click(function(e) {
+	$('.CerrarMenuPrincipal').on('click', function(e) {
 		e.preventDefault();
 		let Menu = $('body > nav');
-		let Overlay = $('body > div#nav_overlay');
+		let Overlay = $('body > a#nav_overlay');
 		Menu.removeClass('active');
 		Menu.fadeToggle('fast', 'linear');
 		Overlay.fadeOut(1200, function() {
