8ed4a8f12dec21883dd01f11cf756f30e203f114
Alejandro Marín Gómez <alejandromaringomez@hotmail.com>
2018-11-19 16:53:58 +0100

    Modificaciones varias.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,39 @@
+/*
+
+$(window).scroll(function() {
+	if($(document).width() > 900) {
+		$(".frozen_top").css("top",Math.max(130,$(this).scrollTop()));
+		if($(this).scrollTop() > 135) {
+		$(".frozen_top").css("margin-top","-95px");
+		$("#task_flyout").css("top","53px");
+		} else {
+		$(".frozen_top").css("margin-top","-5px");
+		$("#task_flyout").css("top","33px");
+		}
+	}
+});
+
+*/
+
 $(document).ready(function() {
 	
 	// Menu principal
+	// Por Alejandro Marín Gómez
 	
 	$('#icono-menu').click(function(e) {
 		e.preventDefault();
-		if ($(this).hasClass('active')){
-			$(this).removeClass('active');
-			$(this).css('background-image', 'url(../img/burguer.svg)');
-			$(this).empty();
-			$('body > nav').fadeToggle('fast', 'linear');
+		let Boton = $(this);
+		let Menu = $('body > nav');
+		if (Boton.hasClass('active')){
+			Boton.removeClass('active');
+			Boton.css('background-image', 'url("' + BaseUrl + '/img/burguer.svg")');
+			Boton.empty();
+			Menu.fadeToggle('fast', 'linear');
 		} else {
-			$(this).addClass('active');
-			$(this).css('background-image', 'none');
-			$(this).html('<i class="fa fa-close fa-2x"></i>');
-			$('body > nav').fadeToggle('fast', 'linear');
+			Boton.addClass('active');
+			Boton.css('background-image', 'none');
+			Boton.html('<i class="fa fa-close fa-2x"></i>');
+			Menu.fadeToggle('fast', 'linear');
 		}
 	});
 	
